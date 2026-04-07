c5bac2063adf81cea2ea462e2557d62a54406f6d
Amanda <amandajuhlsilberg@hotmail.com>
2026-04-07 12:27:38 +0200

    Test Added

---
+++ Calculator/Test/Test1.cs
@@ -0,0 +1,17 @@
+﻿using Calculator;
+
+namespace Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void AddNumbers()
+        {
+        // Arrange 
+
+
+
+        }
+    }
+}
